f1d7f8a946d46921870a259cbf0450b1d508feb9
Andrei Sorokin <Andrei_Sorokin2@epam.com>
2024-02-21 18:10:12 +0100

    uv in update reqs

--- {{ cookiecutter.project_slug }}/Makefile
+++ {{ cookiecutter.project_slug }}/Makefile
@@ -22,8 +22,8 @@ ver-release:
 reqs:
 	pre-commit autoupdate
 	bash ./scripts/compile_requirements.sh
-	pip install -r requirements.txt
-	pip install -r requirements.dev.txt
+	uv pip install -r requirements.txt
+	uv pip install -r requirements.dev.txt
 
 .PHONY: docs # mark as phony so it always runs even we have a docs folder
 .HELP: docs  ## Docs preview English
--- {{ cookiecutter.project_slug }}/scripts/compile_requirements.sh
+++ {{ cookiecutter.project_slug }}/scripts/compile_requirements.sh
@@ -3,14 +3,11 @@
 # Pin current dependencies versions.
 #
 
-rm -f requirements.txt
-rm -f requirements.dev.txt
-
 # pin test / lint / docs dependencies for reproducibility
-pip-compile requirements.dev.in
+uv pipcompile requirements.dev.in
 
 # pin requirements.in versions just as reference for potential incapability bugs in future
-pip-compile requirements.in
+uv pipcompile requirements.in
 
 # do not pin dependencies in the package
 scripts/include_pyproject_requirements.py requirements.in
