30212ac797fef9952c0532435699c1d85062fabf
Andrei Sorokin <Andrei_Sorokin2@epam.com>
2024-03-12 12:45:46 +0100

    migrated from lazydocs to mkdocstrings

---
+++ {{ cookiecutter.package_name }}/docs/src/en/reference.md
@@ -0,0 +1,4 @@
+::: {{ cookiecutter.project_slug }}
+    options:
+      heading_level: 2
+      show_submodules: true
--- {{ cookiecutter.package_name }}/scripts/docstrings.sh
+++ {{ cookiecutter.package_name }}/scripts/docstrings.sh
@@ -4,10 +4,7 @@
 # make a copy for all languages
 #
 
-lazydocs \
-    --output-path="./docs/src/en/api-reference" \
-    --overview-file="index.md" \
-    --src-base-url="https://github.com/andgineer/{{ cookiecutter.package_name }}/blob/master/" \
-    src/{{ cookiecutter.project_slug }}
-
-cp -r ./docs/src/en/api-reference ./docs/src/ru/api-reference
+# I did not find more intelligent way have API Docs for all languages
+for lang in ru; do
+  cp ./docs/src/en/reference.md ./docs/src/$lang/reference.md
+done
