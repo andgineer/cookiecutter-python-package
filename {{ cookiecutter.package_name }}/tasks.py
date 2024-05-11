2b4af3ac4e71ad44bca2f0954299ab99a71e1cff
Andrei Sorokin <Andrei_Sorokin2@epam.com>
2024-05-11 18:09:19 +0200

    docker build tasks factory

--- {{ cookiecutter.package_name }}/tasks.py
+++ {{ cookiecutter.package_name }}/tasks.py
@@ -1,3 +1,4 @@
+import os
 import shutil
 import sys
 
@@ -20,6 +21,10 @@ def get_allowed_doc_languages():
 
 ALLOWED_DOC_LANGUAGES = get_allowed_doc_languages()
 ALLOWED_VERSION_TYPES = ["release", "bug", "feature"]
+
+DOCKER_NAME = "{{ cookiecutter.package_name }}"  # default name for Docker image
+DOCKER_FOLDERS = {"": "."}  # <image name>: <build context folder>
+BUILD_TASK_PREFIX = 'build'
 
 
 @task
@@ -91,9 +96,32 @@ def pre(c):
     """Run pre-commit checks"""
     c.run("pre-commit run --verbose --all-files")
 
+    def docker_build_task_factory(name, target_dir):
+        @task
+        def docker_build(c):
+            """Build Docker image with local setup script."""
+            shared_scripts_dir = "../../docker-scripts"
+            scripts_copy_dir = os.path.join(target_dir, '.setup-scripts')
+            try:
+                args = ""
+                if os.path.exists(shared_scripts_dir):
+                    os.makedirs(scripts_copy_dir, exist_ok=True)
+                    shutil.copytree(shared_scripts_dir, scripts_copy_dir, dirs_exist_ok=True)
+                    args = f"--build-arg SSL_CERT_FILE=/usr/local/share/ca-certificates/custom_cacert.crt"
+                args += f" -t {DOCKER_NAME if name == BUILD_TASK_PREFIX else name.split('-')[-1]}"
+                c.run(f"docker build {args} {target_dir}")
+            finally:
+                if os.path.exists(scripts_copy_dir):
+                    shutil.rmtree(scripts_copy_dir)
+                    pass
 
-namespace = Collection.from_module(sys.modules[__name__])
+        return docker_build
+
+    namespace = Collection.from_module(sys.modules[__name__])
 for name in ALLOWED_VERSION_TYPES:
     namespace.add_task(ver_task_factory(name), name=f"ver-{name}")
 for name in ALLOWED_DOC_LANGUAGES:
     namespace.add_task(docs_task_factory(name), name=f"docs-{name}")
+for name, folder in DOCKER_FOLDERS.items():
+    task_name = f"{BUILD_TASK_PREFIX}-{name}" if name else BUILD_TASK_PREFIX
+    namespace.add_task(docker_build_task_factory(task_name, folder), name=task_name)
