9f3b30127ce3217a3c733d1d75dae9a30373c03c
andgineer <Andrei_Sorokin2@epam.com>
2024-09-13 06:08:43 +0200

    fixed build-docs

--- {{ cookiecutter.package_name }}/scripts/build-docs.sh
+++ {{ cookiecutter.package_name }}/scripts/build-docs.sh
@@ -7,7 +7,10 @@
 
 for lang in en ru; do  # en should be the first language as it clears the root of the site
     scripts/docs-render-config.sh $lang
-    cp -r ./docs/src/en/images/ ./docs/src/$lang/images/
+    if [ $lang != "en" ]; then
+      cp -r ./docs/src/en/images/ ./docs/src/$lang/images/
+      cp ./docs/src/en/reference.md ./docs/src/ru/reference.md
+    fi
     mkdocs build --dirty --config-file docs/_mkdocs.yml
     rm docs/_mkdocs.yml
 done
