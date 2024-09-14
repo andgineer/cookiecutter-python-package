08caed36cf1583ec12e422581678116810e41772
andgineer <Andrei_Sorokin2@epam.com>
2024-09-14 09:55:23 +0200

    check UV/Virtual env installed

--- {{ cookiecutter.package_name }}/activate.sh
+++ {{ cookiecutter.package_name }}/activate.sh
@@ -26,27 +26,42 @@ if [[ ! -d ${VENV_FOLDER} ]] ; then
     unset CONDA_PREFIX  # if conda is installed, it will mess with the virtual env
 
     echo -e $CYAN"Creating virtual environment for python in ${VENV_FOLDER}"$NC
+    START_TIME=$(date +%s)
+
+    # Check if the required Python version is installed
+    if ! command -v python${PRIMARY_PYTHON_VERSION} &> /dev/null; then
+        echo -e $RED"Error: Python ${PRIMARY_PYTHON_VERSION} is not installed."$NC
+        echo -e $YELLOW"Please install Python ${PRIMARY_PYTHON_VERSION} before proceeding."$NC
+        echo -e $YELLOW"You can download it from https://www.python.org/downloads/"$NC
+        return 1
+    fi
     {% if cookiecutter.uv %}
-    if uv venv ${VENV_FOLDER} --python=python${PRIMARY_PYTHON_VERSION}; then
+    if command -v uv &> /dev/null; then
+        if uv venv ${VENV_FOLDER} --python=python${PRIMARY_PYTHON_VERSION}; then
     {% else %}
-    if virtualenv ${VENV_FOLDER} --python=python${PRIMARY_PYTHON_VERSION}; then
-      python -m venv  ${VENV_FOLDER}
+    if command -v virtualenv &> /dev/null; then
+        if virtualenv ${VENV_FOLDER} --python=python${PRIMARY_PYTHON_VERSION}; then
+            python -m venv ${VENV_FOLDER}
     {% endif %}
-      START_TIME=$(date +%s)
+            . ${VENV_FOLDER}/bin/activate
+            {% if cookiecutter.uv %}uv {% endif %}pip install --upgrade pip
+            {% if cookiecutter.uv %}uv {% endif %}pip install -r requirements.dev.txt
 
-      . ${VENV_FOLDER}/bin/activate
-      {% if cookiecutter.uv %}uv {% endif %}pip install --upgrade pip
-      {% if cookiecutter.uv %}uv {% endif %}pip install -r requirements.dev.txt
-
-      END_TIME=$(date +%s)
-      echo "Environment created in $((END_TIME - $START_TIME)) seconds"
+            END_TIME=$(date +%s)
+            echo "Environment created in $((END_TIME - $START_TIME)) seconds"
+        else
+            echo -e $RED"Error creating virtual environment. Please check the output above for more details."$NC
+            return 1
+        fi
     else
-      {% if cookiecutter.uv %}
-      echo -e $RED"Error to create virtual env. Do you have Astral's UV installed ( https://github.com/astral-sh/uv )?"$NC
-      {% else %}
-      echo -e $RED"Error to create virtual env. Do you have virtualenv installed?"$NC
-      {% endif %}
-      return 1
+        {% if cookiecutter.uv %}
+        echo -e $RED"Error: Astral's UV is not installed."$NC
+        echo -e $YELLOW"Please install UV from https://github.com/astral-sh/uv before proceeding."$NC
+        {% else %}
+        echo -e $RED"Error: Virtualenv is not installed."$NC
+        echo -e $YELLOW"Please install Virtualenv from https://virtualenv.pypa.io/en/latest/ before proceeding."$NC
+        {% endif %}
+        return 1
     fi
 else
     echo -e $CYAN"Activating virtual environment ..."$NC
