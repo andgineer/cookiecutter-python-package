d57e8bcfc4c72b7312fe6e6e1140916e56e21814
andgineer <Andrei_Sorokin2@epam.com>
2025-12-28 06:40:05 +0100

    suppresed false positive typing errors in tasks

--- {{ cookiecutter.package_name }}/tasks.py
+++ {{ cookiecutter.package_name }}/tasks.py
@@ -131,12 +131,14 @@ def docker_build_task_factory(name, target_dir):
 
 namespace = Collection.from_module(sys.modules[__name__])
 for name in ALLOWED_VERSION_TYPES:
-    namespace.add_task(ver_task_factory(name), name=f"ver-{name}")  # type: ignore[arg-type]
+    namespace.add_task(ver_task_factory(name), name=f"ver-{name}")  # type: ignore[bad-argument-type]
 for name in ALLOWED_DOC_LANGUAGES:
-    namespace.add_task(docs_task_factory(name), name=f"docs-{name}")  # type: ignore[arg-type]
+    namespace.add_task(docs_task_factory(name), name=f"docs-{name}")  # type: ignore[bad-argument-type]
 
 {% if cookiecutter.docker %}
 for name, folder in DOCKER_FOLDERS.items():
     task_name = f"{BUILD_TASK_PREFIX}-{name}" if name else BUILD_TASK_PREFIX
-    namespace.add_task(docker_build_task_factory(task_name, folder), name=task_name)
+    namespace.add_task(
+        docker_build_task_factory(task_name, folder), name=task_name
+    )  # type: ignore[bad-argument-type]
 {% endif %}
