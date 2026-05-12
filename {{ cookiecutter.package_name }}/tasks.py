c2eb1d82b04a4b5f4f37fbaaeeeacf44a16cb026
andgineer <Andrei_Sorokin2@epam.com>
2026-05-12 13:44:10 +0200

    mkdocs -> zensical

--- {{ cookiecutter.package_name }}/tasks.py
+++ {{ cookiecutter.package_name }}/tasks.py
@@ -1,24 +1,23 @@
 import os
 import shutil
 import sys
+from contextlib import contextmanager
+from pathlib import Path
 
 from invoke import task, Context, Collection
 import time
 
 
+DOCS_PATH = Path("docs")
+DOCS_SRC_PATH = DOCS_PATH / 'src'
+
+
 def get_allowed_doc_languages():
-    build_docs_file_name = "scripts/build-docs.sh"
-    try:
-        with open(build_docs_file_name, "r") as f:
-            for line in f:
-                if "LANGUAGES=" in line:
-                    value = line.split("=", 1)[1].strip()
-                    value = value.strip('"').strip("'")
-                    langs = value.split()
-                    return [lang.strip() for lang in langs]
-    except FileNotFoundError:
-        print(f"No {build_docs_file_name} file found")
-    return ["en", "bg", "de", "es", "fr", "ru"]  # default
+    """Detect languages as subfolders in docs/src/
+
+    Ensure `en` is always first.
+    """
+    return ['en'] + [f.name for f in DOCS_SRC_PATH.iterdir() if f.is_dir() and f.name != "en"]
 
 
 ALLOWED_DOC_LANGUAGES = get_allowed_doc_languages()
@@ -84,15 +83,53 @@ def reqs(c: Context):
     {% endif %}{% endif %}
     
 
+@contextmanager
+def docs_rendered(language: str):
+    """Render docs sources for language specified.
+
+    Copy language agnostic assets from en to non-en folders.
+    Substitute language and site dir in config copy.
+
+    Returns config copy path.
+    """
+    config_template_path = DOCS_PATH / "mkdocs.yml"
+    common_path = DOCS_PATH / "common"
+    src_path = DOCS_SRC_PATH / language
+
+    build_docs_path = Path('build') / "docs"
+    build_config_path = build_docs_path / "mkdocs.yml"
+    build_src_path = build_docs_path / "src" / language
+    site_dir = Path("site") if language == "en" else Path("site") / language
+
+    config = config_template_path.read_text()
+    config = config.replace("LANGUAGE", language)
+    config = config.replace("SITE_DIR", str(site_dir))
+
+    build_docs_path.mkdir(parents=True, exist_ok=True)
+    build_config_path.write_text(config)
+    shutil.rmtree(build_src_path, ignore_errors=True)
+    shutil.copytree(src_path, build_src_path)
+    shutil.copytree(common_path, build_src_path, dirs_exist_ok=True)
+    yield build_config_path
+
+
 def docs_task_factory(language: str):
     @task
     def docs(c: Context):
         """Docs preview for the language specified."""
-        c.run("open -a 'Google Chrome' http://127.0.0.1:8000/{{ cookiecutter.package_name }}/")
-        c.run(f"scripts/build-docs.sh --copy-assets {language}")
-        c.run("mkdocs serve -f docs/_mkdocs.yml")
-
+        with docs_rendered(language) as config_copy_path:
+            port = 8001
+            c.run(f"open -a 'Google Chrome' http://127.0.0.1:{port}")
+            c.run(f"zensical serve --config-file {config_copy_path} --dev-addr localhost:{port}")
     return docs
+
+
+@task
+def build_docs(c: Context):
+    """Build docs in docs/site/."""
+    for language in ALLOWED_DOC_LANGUAGES:
+        with docs_rendered(language) as config_copy_path:
+            c.run(f"zensical build --config-file {config_copy_path}")
 
 
 @task
